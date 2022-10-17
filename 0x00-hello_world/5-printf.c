1efbfdab56cc62f16d32cdba2f18b1367f9c900e
k-dels <delali.klu001@gmail.com>
2022-10-17 12:11:05 -0700

    More C basics

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main -entry point
+ * main -Entry point
  *
- * Description: A C program that prints using printf
+ * Description: A c program that prints with printf
  *
- * return: Always 0 (success)
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
