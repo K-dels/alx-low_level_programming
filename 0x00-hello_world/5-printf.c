cd1f5534c71816fe3bf683313aded660ea8f97cd
k-dels <delali.klu001@gmail.com>
2022-10-17 09:21:42 -0700

    Using printf to print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Description: A C program that prints using printf
+ *
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
