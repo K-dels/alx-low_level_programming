7de7d1b8aa3f424d09272dd6d4ecfa3631444921
k-dels <delali.klu001@gmail.com>
2022-10-20 09:49:12 -0700

    betty fix for prining largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
-*main - prints the largest of 3 integers
+*largest_number- prints the largest of 3 integers
+*@a: first integer
+*@b: second integer
+*@c: third integer
 *Return: largest number
 */
 
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	
+
 	if (a >= b && a >= c)
 	{
 		largest = a;
